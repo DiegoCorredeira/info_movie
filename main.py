d98a85a72abba7e087e4cc638a1fcd57a905c7ea
Diego Corredeira <dgcostapinto@gmail.com>
2023-04-01 11:15:14 -0300

    iniciado GUI

--- main.py
+++ main.py
@@ -1,42 +1,79 @@
 import requests
-from PIL import Image
+from PIL import Image, ImageTk
 from translate import Translator
 from io import BytesIO
-import json
+import tkinter as tk
 
+
+'''
+Proximos passos: 
+Criar GUI
+Traduzir nome do filme de en para pt-br
+
+'''
 translator = Translator(from_lang="en", to_lang='pt-br')
-movie_title = input('Informe um filme: ')
-api_movie = f'http://www.omdbapi.com/?apikey=30c395bf&t={movie_title}'
 
-response = requests.get(api_movie)
-data = response.json()
+def movieSearch():
+    movie_title = entry.get()
+    api_movie = f'http://www.omdbapi.com/?apikey=30c395bf&t={movie_title}'
+    
+    response = requests.get(api_movie)
+    data = response.json()
+    
+    title = data['Title']
+    year = data['Year']
+    genre = data['Genre']
+    plot = data['Plot']
+    poster = data['Poster']
+    
+    response_poster = requests.get(poster)
+    if 'image' in response_poster.headers['content-type']:
+        img = Image.open(BytesIO(response_poster.content))
+        img.save('poster.jpg')
+        imagem = ImageTk.PhotoImage(img)
+        image_label.config(image=imagem)
+        image_label.image = imagem
+    else:
+        print('A URL não contém uma imagem válida.')
+        image_label.config(image=None)
+        image_label.image = None
+    try:
+        translated_plot = translator.translate(plot)
+    except StopIteration:
+        print('Não foi possível traduzir a sinopse do filme.')
+        translated_plot = ''
+    except ValueError:
+        print('A sinopse do filme é inválida.')
+        translated_plot = ''
+    except Exception as e:
+        print(f'Erro desconhecido: {str(e)}')
+        resultado = ''
+    msg = f'Titulo: {title}\nAno de Lançamento: {year}\nGenero: {genre}\nSinopse: {translated_plot}'
+    result.config(text=msg)
 
-title = data['Title']
-year = data['Year']
-runtime = data['Runtime']
-genre = data['Genre']
-plot = data['Plot']
-poster = data['Poster']
-director = data['Director']
+janela = tk.Tk()
+janela.title('Info Movie')
 
-response_poster = requests.get(poster)
-if 'image' in response_poster.headers['content-type']:
-    img = Image.open(BytesIO(response_poster.content))
-    img.save('poster.jpg')
-    img.show()
-else:
-    print('A URL não contém uma imagem válida.')
+font = ('Arial', 14)
 
-try:
-    translated_plot = translator.translate(plot)
-except StopIteration:
-    print('Não foi possível traduzir a sinopse do filme.')
-    translated_plot = ''
-except ValueError:
-    print('A sinopse do filme é inválida.')
-    translated_plot = ''
-except Exception as e:
-    print(f'Erro desconhecido: {str(e)}')
-    resultado = ''
-msg = f'Titulo: {title}\nAno de Lançamento: {year}\nGenero: {genre}\nSinopse: {translated_plot}'
-print(msg)
+entry = tk.Entry(janela)
+entry.pack()
+
+entry.bind("<Return>", lambda x: movieSearch())
+
+search = tk.Button(janela, text='Pesquisar Filme', command=movieSearch)
+search.pack()
+
+result = tk.Label(janela, text=None, wraplength=500, font=font)
+result.pack()
+
+image_label = tk.Label(janela, image='')
+image_label.pack()
+
+janela.mainloop()
+
+
+
+
+
+
